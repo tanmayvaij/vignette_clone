a3d746b4345cbbf4a6782bb81b557ba2844f20af
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-11 19:38:46 +0530

    small home page design completed

--- src/components/SectionMainAction.jsx
+++ src/components/SectionMainAction.jsx
@@ -2,14 +2,14 @@ const SectionMainAction = () => {
     return (
         <div id="section_main_action">
 
-            <div className="text-[16px] flex items-center justify-center flex-col">
+            <div>
 
-                <button className="text-white rounded-[50px] px-[30px] py-[15px] bg-[#0066ff] flex items-center justify-around">
+                <button id="buy_btn" className="text-white bg-[#0066ff]">
                     <ion-icon size="large" name="cart" ></ion-icon>
                     <span>BUY VIGNETTE</span>
                 </button>
 
-                <button className="bg-white rounded-[50px] px-[30px] py-[15px] flex items-center justify-around">
+                <button id="check_btn" className="bg-white">
                     <ion-icon size="large" name="information-circle-outline"></ion-icon>
                     <span>CHECK STATUS</span>
                 </button>
